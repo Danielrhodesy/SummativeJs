8e4a9a5d4772ae72730906be6635ef7dd2b2276a
Daniel John Rhodes <932400619@bun2-206-21.local>
2018-06-18 19:43:00 +1200

    Finished Page Transitions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+$(document).ready(function() {
 
 //Fade in start up main-page
 
@@ -5,6 +6,80 @@ $('#animsition-mainpage').animsition();
 
 // Animsition through pages
 
-$(function() {
-  
+  //Main page to page 2
+  $('.here-button').click(function() {
+    $('#mainpage-div').fadeOut(function() {
+      $('#mainpage-div').hide(function() {
+        $('#page2-div').fadeIn();
+      });
+    });
+  });
+
+  // Page 2 to page 3
+  $('.v-button').click(function() {
+    $('#page2-div').fadeOut(function() {
+      $('#page2-div').hide(function() {
+        $('#page3-div').fadeIn();
+      });
+    });
+  });
+
+  // Page 3 to page 4
+  $('.ph-button').click(function() {
+    $('#page3-div').fadeOut(function() {
+      $('#page3-div').hide(function() {
+        $('#page4-div').fadeIn();
+      });
+    });
+  });
+
+  // Page 4 to page 5
+  $('.m-button').click(function() {
+    $('#page4-div').fadeOut(function() {
+      $('#page4-div').hide(function() {
+        $('#page5-div').fadeIn();
+      });
+    });
+  });
+
+
+//Back button
+
+  // Back from page 2 to main
+  $('.v-backbutton-wrapper').click(function(){
+    $('#page2-div').fadeOut(function(){
+      $('#page2-div').hide(function(){
+        $('#mainpage-div').fadeIn();
+      });
+    });
+  });
+
+  //Back from page 3 to page 2
+  $('.ph-backbutton-wrapper').click(function(){
+    $('#page3-div').fadeOut(function(){
+      $('#page3-div').hide(function(){
+        $('#page2-div').fadeIn();
+      });
+    });
+  });
+
+  //Back from page 4 to page 3
+  $('.m-backbutton-wrapper').click(function(){
+    $('#page4-div').fadeOut(function(){
+      $('#page4-div').hide(function(){
+        $('#page3-div').fadeIn();
+      });
+    });
+  });
+
+  //Back from page 5 to page 4
+  $('.c-backbutton-wrapper').click(function(){
+    $('#page5-div').fadeOut(function(){
+      $('#page5-div').hide(function(){
+        $('#page4-div').fadeIn();
+      });
+    });
+  });
+
+
 });
